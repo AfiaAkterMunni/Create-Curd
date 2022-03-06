ede15fafddaf0faebf9f5f9d3081e91c1fa79316
AfiaAkterMunni <afiamunni03@gmail.com>
2022-03-07 01:00:44 +0600

    code refactor.

--- create.php
+++ create.php
@@ -3,16 +3,12 @@
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_curd', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
 ?>
-
-
-
-
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,10 +17,11 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="app.css">
     <title>Products CURD</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <h1>Create New Product</h1>
-    
+
     <form action="">
         <div class="form-group">
             <label>Product Image</label>
@@ -36,6 +33,7 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             <label>Product Title</label>
             <input type="text" class="form-control">
         </div>
+
         <br>
         <div class="form-group">
             <label>Product Description</label>
@@ -47,6 +45,7 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             <input type="number" step=".01" class="form-control">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-  </body>
+    </form>
+</body>
+
 </html>
