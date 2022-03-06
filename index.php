41a33278acd205dd5cb8be9b213b0f189759ebba
AfiaAkterMunni <afiamunni03@gmail.com>
2022-03-07 01:25:12 +0600

    folder structure modify

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="stylesheet" href="app.css">
+  <link rel="stylesheet" href="css/app.css">
   <title>Products CRUD</title>
 </head>
 
